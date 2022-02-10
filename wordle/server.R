246c1e891d292ac6b77cd98486ac9ae0a4e3b16c
yao-dataventures <yao@dataventures.nz>
2022-02-10 05:52:44 +0000

    Show the word at the end of the game.

--- wordle/server.R
+++ wordle/server.R
@@ -11,11 +11,26 @@ shinyServer(function(input, output, session) {
         result <- check_word(real = word_today, guess = word_input)
         tries$results <- c(tries$results, list(result))
         
+        if (any(result$results == "correct")){
+            disable(id = "myword")
+            showModal(modalDialog(
+                title = "Congratulations!",
+                paste0("Today's word is '", word_today, "'."),
+                easyClose = TRUE,
+                footer = NULL
+            ))
+        } 
+        
+        
         if (length(tries$results) == 5){
             disable(id = "myword")
+            showModal(modalDialog(
+                title = "Oops!",
+                paste0("Today's word is '", word_today, "'."),
+                easyClose = TRUE,
+                footer = NULL
+            ))
         }
-        
-        
     })
     
     output$try_results <- renderPrint({
